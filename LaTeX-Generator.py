5e226d520d1a5247a3aa58fc1b512f30f52c37b8
Matthew N <matmath8m8@gmail.com>
2022-11-10 19:23:31 -0600

    Added iterateMatrix, roundMatrix, convertInts

--- LaTeX-Generator.py
+++ LaTeX-Generator.py
@@ -3,11 +3,20 @@
 # The premise is that these functions will deal with a list of strings which can be printed out as the LaTeX code.
 
 import numpy as np
+import math
 
+def iterateMatrix(matrix, function = lambda x: x):
+    matrix = np.array(matrix) # Making sure it's a numpy array
+    for idx, val in np.ndenumerate(matrix):
+        matrix[idx] = function(val) # Replaces the value in the matrix with the value after it is input to the function provided.
+                                    # NOTE: numpy arrays are mutable, so this will actually change the original array.
+    return matrix
 
+def roundMatrix(matrix, numDigits):
+    return iterateMatrix(matrix, lambda x: round(x, numDigits)) # Iterates through the matrix, rounding each element, then returning it.
 
-
-
+def convertIntsInMatrix(matrix):
+    return iterateMatrix(matrix, lambda x: int(x) if x == math.floor(x) else x) # If the entry is the same value as its floor, it is an integer, so it converts it.
 
 def isListOrArray(input):
     if type(input) in [type([]), type(np.arr([]))]:
@@ -20,6 +29,10 @@ def isListOrArray(input):
 # This only prints out 2D arrays.
 # Prints out the LaTeX code for the matrix.
 def latexMatrix(matrix, packages, matrixType = "b"): 
+    """
+    Takes a matrix and converts it into a string that contains the LaTeX code for displaying the matrix provided. 
+    matrixType must be "b", "p", "B", "v", or "V". Packages are required to make sure amsmath is being used.
+    """
     if type(matrixType) != type("") or matrixType not in ["b", "p", "B", "v", "V"]: # list of common matrix types of latex
         raise TypeError("Matrix type not in list of possible types")
     if "amsmath" not in packages:
@@ -38,7 +51,8 @@ def latexMatrix(matrix, packages, matrixType = "b"):
     for row in matrix:
         if matrix.ndim == 2: # type(row) == type([]):
             # Add each element in the row to the list
-            for element in row:
+            for elementIdx in range(len(row)):
+                element = row[elementIdx] # if I don't do it this way, it won't work with numpy arrays
                 latexCode.append(str(element)) # there can be any number of columns
 
                 # Only add the "&" symbol between the elements, not after the last one
@@ -48,10 +62,10 @@ def latexMatrix(matrix, packages, matrixType = "b"):
             
             print(matrix[-1])
             print(row)
-            print(row == matrix[-1])
+            print(row.all == matrix[-1].all)
 
             # This conditional prevents it from adding "\\" after the last row
-            if row != matrix[-1]:
+            if row.all != matrix[-1].all:
                 # After each row except for the last one, you need to add "\\"
                 latexCode.append("\\\\")
         else:
@@ -88,7 +102,7 @@ def cofactor(matrix):
     inv = inverse(matrix)
     
     if determinant != 0:
-        return transpose(inv) * determinant
+        return roundMatrix(transpose(inv) * determinant, 10) # Need to round because sometimes the floating point operations can result in weird decimals
     else:
         raise ValueError("Determinant was zero - matrix was singular")
 
@@ -100,6 +114,18 @@ def cofactor(matrix):
 packageList = ["amsmath"]
 testingMatrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 
-print(latexMatrix(testingMatrix, packageList))
 
-print(latexMatrix(cofactor(testingMatrix), packageList))
+print(type(np.array(testingMatrix)[0][0]))
+
+print(iterateMatrix(np.array(testingMatrix), lambda x: 2 * x))
+
+v = roundMatrix([1.000000000000005, 3.0045, 10.05], 10)
+
+print(v[0])
+
+print(convertIntsInMatrix(v))
+
+print(convertIntsInMatrix(cofactor(testingMatrix)))
+# print(latexMatrix(testingMatrix, packageList))
+
+# print(latexMatrix(cofactor(testingMatrix), packageList))
